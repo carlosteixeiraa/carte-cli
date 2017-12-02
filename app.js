c98e48ad9175ad6c56b3e3a14cfcaea94fd62e31
carlosteixeiraa <carlosteixeira.email@gmail.com>
2017-12-02 10:56:01 +0000

    -.

--- app.js
+++
@@ -1,8 +0,0 @@
-const program = require('commander');
-
-program
-  .version('0.0.1')
-  .option('-f, --framework','option description')
-  .option('-c, --colors','we can have as many options as we want')
-  .option('-l, --librarys <required>','required user input')
-  .parse(process.argv); 
